0128909785d5f24e530af9a84443fa01b181a3d4
Adibul Atsfar <adibulatsfar@gmail.com>
2026-07-23 19:21:42 +0700

    FIX

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use App\Models\Profile;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
@@ -12,6 +13,10 @@ class AppServiceProvider extends ServiceProvider
 
     public function boot(): void
     {
+        if (config('app.env') === 'production') {
+            URL::forceScheme('https');
+        }
+
         View::composer('layouts.app', function ($view) {
             $view->with('profil', Profile::first());
         });
